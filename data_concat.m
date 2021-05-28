82811c8fd82d71687a008a4ef6879f6eef2888e2
Yongwei Wang <npuyw.wang@gmail.com>
2021-05-28 00:08:43 -0700

    add normalization

--- data_concat.m
+++ data_concat.m
@@ -2,6 +2,7 @@
 % each file contains fi,i=1,..,4.
 % f3 contains imagery number, use real part only;
 % f4 has different dimension and cannot be concatented with f1-f3. 
+% Also, perform normalization for f1 to f3, respectively.
 % Yongwei Wang
 % May 27, 2021
 
@@ -9,7 +10,7 @@ close all;
 clear; clc
 
 mat_files = dir('*.mat'); 
-save_dir_base = '..\\Feature_data_processed\\'; 
+save_dir_base = '..\\feats\\'; 
 
 for k = 1: length(mat_files)
     file_name = mat_files(k).name;
@@ -20,7 +21,9 @@ for k = 1: length(mat_files)
     end
     
     cur_data_tmp = load(file_name);
-    cur_data = cat(3, cur_data_tmp.f1, cur_data_tmp.f2, real(cur_data_tmp.f3)); 
+    cur_data = cat(3, normalize_tensor(cur_data_tmp.f1), ...
+                    normalize_tensor(cur_data_tmp.f2), ...
+                    normalize_tensor(real(cur_data_tmp.f3))); 
     
     if string(cur_name) == string(pre_name)
         data_comb = cat(1, pre_data, cur_data);  
@@ -31,13 +34,16 @@ for k = 1: length(mat_files)
     pre_data = data_comb;
     pre_name = cur_name; 
     
-    % save data_comb
-    disp(size(data_comb));
-    save_file_path = [save_dir_base, cur_name, '.mat'];
+    folder_name_tmp = split(file_name, '_'); 
+    folder_name = char(folder_name_tmp(2)); 
+    
+    %save data_comb
+    save_file_path = [save_dir_base, folder_name, '.mat'];
     save(save_file_path, 'data_comb');
-        
-%     folder_name_tmp = split(file_name, '_'); 
-%     folder_name = char(folder_name_tmp(2)); 
       
 end
 
+
+function out = normalize_tensor(tensor)
+out = tensor / max(tensor(:)); 
+end
